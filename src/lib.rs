56a347a006493e6b7ec5d33e5da73ad26945d7e5
KantaTamura <tkanta496@gmail.com>
2022-05-31 23:02:15 +0900

    :sparkles: minesweeper model
    add minesweeper struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::collections::HashSet;
+
+type Position = (usize, usize);
+
+struct Minesweeper {
+    width: usize,
+    height: usize,
+    open_fields: HashSet<Position>,
+    mines: HashSet<Position>,
+    flagged_fields: HashSet<Position>,
+}
+
+impl Minesweeper {
+    pub fn new(width: usize, height: usize) -> Minesweeper {
+        Minesweeper {
+            width,
+            height,
+            open_fields: HashSet::new(),
+            mines: HashSet::new(),
+            flagged_fields: HashSet::new(),
+        }
     }
 }
